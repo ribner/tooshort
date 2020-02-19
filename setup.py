9213b0e63d6af083230e4b6521cd158198ade7a6
Elliott Ribner <elliottribner@Elliotts-MacBook-Pro.local>
2020-02-18 23:34:41 -0800

    added gitignore

--- setup.py
+++ setup.py
@@ -2,23 +2,18 @@
 
 from setuptools import setup
 
-# setup(name='too_short',
-#       version='1.0',
-#       # list folders, not files
-#       packages=['too_short',
-#                 'too_short.test'],
-#       scripts=['capitalize/bin/cap_script.py'],
-#       package_data={'capitalize': ['data/cap_data.txt']},
-#       )
-with open("README", 'r') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
+
 setup(
     name='too_short',
     version='1.0',
     long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
     description='Module to simplify supervised ml work with sklearn',
-    author='Elliott Ribner',
     packages=['too_short'],  # same as name # list folders, not files
-    install_requires=['sklearn', 'pandas', 'numpy', 'imblearn',
-                      'unittest', 'collections'],  # external packages as dependencies
+    # external packages as dependencies
+    install_requires=['sklearn', 'pandas', 'numpy',
+                      'imbalanced-learn'],
 )
